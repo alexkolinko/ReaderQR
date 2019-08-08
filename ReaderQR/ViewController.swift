097d4c48b5197b1cab78a9ce5b5ad380d21137c9
alexkolinko <as.kolinko@gmail.com>
2019-08-08 09:29:18 +0300

    Initial Commit

---
+++ ReaderQR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReaderQR
+//
+//  Created by Alexandr on 8/8/19.
+//  Copyright © 2019 Alex Kolinko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
